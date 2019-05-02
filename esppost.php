6a79fb2ca4e0d900a2748c64d36bcfa1f946ef00
brabantduncan <duncan.brabant@student.howest.be>
2019-05-02 18:43:31 +0200

    PHP page to push nodeMCU data to

---
+++ esppost.php
@@ -0,0 +1,10 @@
+<?php
+$Temp = $_POST["temperature"];
+
+$Light = $_POST["light"];
+
+$Write = "<p>Temperature : " .$Temp. " Celsius </p>" ."<p>Light : ".$Light." lux</p>";
+
+file_put_contents('sensor.html', $Write);
+?>
+
