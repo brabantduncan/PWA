01a6682a8f0ffcff9c49f5227180ab95741cc14f
brabantduncan <duncan.brabant@student.howest.be>
2019-05-03 00:05:18 +0200

    m

--- postdemo.php
+++ postdemo.php
@@ -3,12 +3,11 @@ include_once 'DBConnect.php';
 
 OpenCon();
 
-if(!empty($_POST['temp']) && !empty($_POST['light']))
-{
+if (!empty($_POST['temp']) && !empty($_POST['light'])) {
     $temp = $_POST['temp'];
     $light = $_POST['light'];
 
-    $sql = "INSERT INTO logs (light, temp, Date, Time) VALUES ('" .$light. "', '" .$temp. "')";
+    $sql = "INSERT INTO logs (light, temp) VALUES ('" . $light . "', '" . $temp . "')";
 
     if (mysqli_query($conn, $sql) === TRUE) {
         echo "OK";
@@ -18,5 +17,13 @@ if(!empty($_POST['temp']) && !empty($_POST['light']))
 }
 
 CloseCon($conn);
-
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Please</h1>
+</body>
+</html>
